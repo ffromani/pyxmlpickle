a1e93a112476ce655911588ff0817f8a7d66fe2e
Francesco Romani <fromani@redhat.com>
2017-02-16 16:08:35 +0100

    core: implement load() functions

--- xmlpickle.py
+++ xmlpickle.py
@@ -23,19 +23,26 @@ from __future__ import absolute_import
 import xml.etree.ElementTree as ET
 
 
+ROOT = 'pyxmlpickle'
+
+
+class Malformed(Exception):
+    pass
+
+
 class Unpicklable(Exception):
     pass
 
 
-def dump(obj, root=None):
+def dump(obj, strict=False, root=None):
     if root is None:
-        root = ET.Element('pyxmlpickle')
+        root = ET.Element(ROOT)
     parent = ET.SubElement(root, 'value')
-    _dump(obj, parent)
+    _dump(obj, parent, strict=strict)
     return root
 
 
-def _dump(obj, parent):
+def _dump(obj, parent, strict=False):
     if isinstance(obj, dict):
         return _dump_dict(obj, parent)
     if isinstance(obj, list):
@@ -43,25 +50,27 @@ def _dump(obj, parent):
     if (isinstance(obj, tuple) or isinstance(obj, set) or
         isinstance(obj, frozenset)
     ):
-        return _dump_list(obj, parent)
+        if strict:
+            raise Unpicklable()
+        return _dump_list(obj, parent, strict=strict)
     return _dump_type(obj, parent)
 
 
-def _dump_dict(obj, parent):
+def _dump_dict(obj, parent, strict=False):
     node = parent
     node.attrib['type'] = 'dict'
     for key, val in obj.items():
         itemelem = ET.SubElement(node, 'item', key=str(key))
-        _dump(val, itemelem)
+        _dump(val, itemelem, strict=strict)
     return node
 
 
-def _dump_list(obj, parent):
+def _dump_list(obj, parent, strict=False):
     node = parent
     node.attrib['type'] = 'list'
     for idx, val in enumerate(obj):
         itemelem = ET.SubElement(node, 'item', index=str(idx))
-        _dump(val, itemelem)
+        _dump(val, itemelem, strict=strict)
     return node
 
 
@@ -82,8 +91,60 @@ def _dump_type(obj, parent):
     return x
 
 
-def load(node):
-    return object()
+def _load_dict(node):
+    ret = {}
+    for item in node.findall('./item'):
+        key = item.attrib.get('key')
+        ntype = item.attrib.get('type')
+        if key is None or ntype is None:
+            raise Malformed()
+        ret[str(key)] = _load(item, ntype)
+    return ret
+
+
+def _load_list(node):
+    ret = []
+    for item in node.findall('./item'):
+        idx = item.attrib.get('index')
+        ntype = item.attrib.get('type')
+        if idx is None or ntype is None:
+            raise Malformed()
+        ret.append(_load(item, ntype))
+    return ret
+
+
+def _load_type(node, ntype):
+    if ntype is None:
+        raise Malformed()
+    ntype = ntype.lower()
+    if ntype == 'bool':
+        return bool(node.text.lower() == 'true')
+    elif ntype == 'int':
+        return int(node.text)
+    elif ntype == 'float':
+        return float(node.text)
+    elif ntype == 'str':
+        return str(node.text)
+    return Malformed()
+
+
+def _load(node, ntype):
+    if ntype == 'dict':
+        return _load_dict(node)
+    elif ntype == 'list':
+        return _load_list(node)
+    return _load_type(node, node.attrib.get('type'))
+
+
+def load(node, root=None):
+    if root is None:
+        root = ROOT
+    if node.tag != root:
+        raise Malformed()
+    value = node.find('./value')
+    if value is None:
+        raise Malformed()
+    return _load(value, value.attrib.get('type'))
 
 
 def dumps(obj):
